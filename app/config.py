8fe681f2d04c803715cef174774296d094000b7f
Phanice <maureezgaga@gmail.com>
2020-02-03 12:08:36 +0300

    added a child class production config

--- app/config.py
+++ app/config.py
@@ -6,14 +6,14 @@ class Config:
 
 
 
-# class ProdConfig(Config):
-#     '''
-#     Production  configuration child class
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
 
-#     Args:
-#         Config: The parent configuration class with General configuration settings
-#     '''
-#     pass
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
 
 
 # class DevConfig(Config):
